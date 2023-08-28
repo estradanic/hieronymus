fa518f9fb791ecdd8f814575568cf5bbc3a3995c
Nicholas Estrada <github@nicholasjestrada.aleeas.com>
2023-08-28 13:45:09 -0700

    Panels are now resizable

--- src/routes/[chapter=chapter]-[commentary=commentary]/+page.svelte
+++ src/routes/[chapter=chapter]-[commentary=commentary]/+page.svelte
@@ -5,15 +5,44 @@
 	import Verse from "$lib/components/Verse.svelte";
 
 	export let data;
+
 	let commentary: Commentary;
 	let chapter: Chapter;
+	let scriptureDiv: HTMLDivElement;
+	let commentaryDiv: HTMLDivElement;
+	let contentDiv: HTMLDivElement;
+	let startX: number;
+	let startWidth: number;
+
+	const MIN_WIDTH = 300 as const;
+	function drag(e: MouseEvent) {
+		e.preventDefault();
+		const diffX = e.pageX - startX;
+		const newWidth = startWidth + diffX;
+		if (newWidth < MIN_WIDTH || newWidth > contentDiv.offsetWidth - MIN_WIDTH) return;
+
+		scriptureDiv.style.flexBasis = `${newWidth}px`;
+		commentaryDiv.style.flexBasis = `calc(100% - ${newWidth}px)`;
+	}
+
+	function stopDrag() {
+		document.removeEventListener("mousemove", drag);
+		document.removeEventListener("mouseup", stopDrag);
+	}
+
+	function startDrag(e: MouseEvent) {
+		startX = e.pageX;
+		startWidth = scriptureDiv.offsetWidth;
+		document.addEventListener("mousemove", drag);
+		document.addEventListener("mouseup", stopDrag);
+	}
 
 	$: commentary = $page.params.commentary as Commentary;
 	$: chapter = $page.params.chapter as Chapter;
 </script>
 
-<div class="content">
-	<div class="panel scripture">
+<div class="content" bind:this={contentDiv}>
+	<div class="panel scripture" bind:this={scriptureDiv}>
 		<h4 class="title">{chapter.split(":").slice(0, 2).join(" ").replaceAll("_", " ")}</h4>
 		<div class="text">
 			{#if !data.scripture}
@@ -27,8 +56,9 @@
 			{/if}
 		</div>
 	</div>
-	<div class="divider" />
-	<div class="panel">
+	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+	<div role="separator" class="divider" on:mousedown={startDrag} />
+	<div class="panel commentary" bind:this={commentaryDiv}>
 		<CommentarySelector value={commentary} />
 		<div class="text">
 			{#if !data.commentary}
@@ -90,9 +120,18 @@
 		scrollbar-width: none;
 	}
 	.divider {
-		flex-basis: 2rem;
+		flex-basis: 1rem;
 		flex-shrink: 0;
 		flex-grow: 0;
+		cursor: col-resize;
+		transition: opacity 1s;
+		background-color: firebrick;
+		opacity: 0;
+		border: 4px solid #f5f5f5;
+		border-radius: 8px;
+	}
+	.divider:hover {
+		opacity: 1;
 	}
 	.title {
 		width: 100%;
